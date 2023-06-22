72f39a41ddea19e01e970e587447952a4940662d
omar mansy <omarmansy96@gmail.com>
2023-06-22 15:42:46 +0300

    edit the terrain and import fungus pluin

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,15 +6,17 @@ public class Player : MonoBehaviour
 {
 
     public GameObject player;
-    public float axisH,axisV,moveSpeed, runSpeed,jump;
-    private Vector3 vector;
+    public float hAxis,vAxis,moveSpeed, runSpeed,jump;
+    private Vector3 movement;
     Rigidbody rb;
+    public Animator anim;
     
 
     void Start()
     {
-        axisH = Input.GetAxis("Horizontal");
-        axisV = Input.GetAxis("Vertical");
+        rb = GetComponent<Rigidbody>();
+
+     
         
         
     }
@@ -22,10 +24,33 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if(axisH > 0)
-        {
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
+       
+        Move();
 
-        }
         
+
+    }
+    void Move()
+    {
+        movement.z = vAxis;
+        if( Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(new Vector3(movement.x, movement.y, movement.z) * runSpeed * Time.deltaTime);
+            anim.SetFloat("fastRun", vAxis);
+            anim.SetTrigger("sheft");
+        }
+       
+            transform.Translate(new Vector3(movement.x, movement.y, movement.z) * moveSpeed * Time.deltaTime);
+            anim.SetFloat("run",vAxis);
+
+            transform.Rotate(0,hAxis,0);
+       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, jump, 0));
+            anim.SetTrigger("jump");
+        }
     }
 }
